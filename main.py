ecd7183f1451ca0466578318d33e4b0449c248f6
sunkey12 <95144563+sunkey12@users.noreply.github.com>
2023-04-15 19:30:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import boto3
 
 # IAM Credentials
-# access_key = "$AWS_ACCESS_KEY_ID"
-# secret_key = "$AWS_SECRET_ACCESS_KEY"
+ access_key = "$AWS_ACCESS_KEY_ID"
+ secret_key = "$AWS_SECRET_ACCESS_KEY"
 
-access_key = ""
-secret_key = ""
+# access_key = ""
+# secret_key = ""
 
 
 
